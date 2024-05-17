981535747f59c2cf527a4c138d9620876b6909da
Abdarrahmane LIASSE <132529005+AbdarrahmaneLiasse@users.noreply.github.com>
2024-05-17 20:47:14 +0100

    Add files via upload

---
+++ MESROLES.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace WebApplication1
+{
+    public class MESROLES : RoleProvider
+    {
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+        SqlConnection cnx = new SqlConnection(Properties.Settings.Default.cnx);
+        public override string[] GetRolesForUser(string username)
+        {
+            cnx.Open();
+            SqlCommand cmd = new SqlCommand("Select categorie from compte where loginc =@l", cnx);
+            cmd.Parameters.AddWithValue("@l", username);
+            String Resulta = cmd.ExecuteScalar().ToString();
+            cnx.Close();
+            return new String[] { Resulta };
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object usernames { get; set; }
+    }
+}
